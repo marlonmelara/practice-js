ea44e056ada472bdfd9f36b91ce1a9461fb532d6
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2026-07-16 15:49:24 -0400

    feat: add new exercises

---
+++ src/js/01-times-table.js
@@ -0,0 +1,29 @@
+/**
+ * Ejercicio 1.
+ * Dado un número, devuelve su tabla de multiplicar completa.
+ */
+
+// Solución 1:
+
+const generateTables = (number) => {
+  console.log(`Tabla del ${number}`);
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${number} x ${i} = ${number * i}`);
+  }
+};
+
+generateTables(2);
+
+// Solución 2 (Responsabilidad única)
+
+const getMultiplicationTable = (n) => {
+  const result = [];
+  for (let i = 1; i <= 10; i++) {
+    result.push(`${n} x ${i} = ${n * i}`);
+  }
+  return result.join("\n");
+};
+
+const number = 5;
+console.log(`Tabla del ${number}`);
+console.log(getMultiplicationTable(number));
